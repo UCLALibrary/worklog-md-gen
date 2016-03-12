56a69ae1ee835da577e6d02ce31f301b064e3642
Mark Matney <mmatney@library.ucla.edu>
2016-03-11 18:58:28 -0800

    Fix incorrect grouping of weeks

--- worklog-md-gen.js
+++ worklog-md-gen.js
@@ -3,8 +3,10 @@ var generateWorklogArgs = process.argv.slice(2);
     var padWithLeadingZeros = function (n) {
         return (n < 10) ? ('0' + n) : ('' + n);
     };
-    var monthZeroIndexed = (month + 11) % 12;
-    var nDaysInMonth = new Date(year, month, 0).getDate();
+    var dow = ['sun', 'mon', 'tue', 'wed', 'thr', 'fri', 'sat'];
+    var monthZeroIndexed = month - 1;
+    var nDaysInMonth = /8|3|5|10/.test(--month)?30:month==1?(!(year%4)&&year%100)||!(year%400)?29:28:31;
+    month++;
     var str = ''
     var entry;
     for (var i = 1; i <= nDaysInMonth; i++) {
@@ -13,7 +15,7 @@ var generateWorklogArgs = process.argv.slice(2);
         var dayOfWeek = new Date(year, monthZeroIndexed, i).getDay();
 
         if (dayOfWeek !== 0 && dayOfWeek !== 6) {
-            if (dayOfWeek === 5 && i !== nDaysInMonth) {
+            if (dayOfWeek === 5 && i < nDaysInMonth - 2) {
                 entry = '--\n\n';
             }
             else {
